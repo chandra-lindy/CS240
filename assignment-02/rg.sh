a58b711d041421ce48135cdad4e4c99a6fff2fdc
Chandra Lindy <chandra.lindy@gmail.com>
2022-09-16 17:13:27 -0700

    started gdb portion of assignment

---
+++ assignment-02/rg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Program: Right Trianlges
+# Author: Chandra Lindy
+
+# Purpose: compile C, assemble asm files and run the resulting executable
+
+# Clear any previously compiled outputs
+rm *.o
+rm *.lis
+rm *.out
+
+echo "Assemble triangle.asm"
+nasm -f elf64 -l triangle.lis -o triangle.o triangle.asm -gdwarf
+
+echo "Compile pythagoras.c"
+gcc -Wall -fno-pie -no-pie pythagoras.c triangle.o -o right-triangles.out -g
+
+echo "Run the Right Triangles Program:\n\n"
+gdb ./right-triangles.out
+
+echo "Script file has terminated."
+
+# Clear any new compiled outputs to keep environment clean
+#rm *.o
+#rm *.lis
+#rm *.out
