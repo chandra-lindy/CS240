8e96224ed4ad96e1f76bbc6bcf6dfa2b1fbe3c74
Chandra Lindy <chandra.lindy@gmail.com>
2022-10-09 11:14:51 -0700

    main.c works as specified

--- assignment-03/display_array.cpp
+++ assignment-03/display_array.cpp
@@ -1,18 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <time.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <iostream>
 
-extern "C" double compare();
-
-int main(int argc, char *argv[])
+extern "C"
+int display_array(int argc, char *argv[])
 {
-    printf("Welcome to Floating Points Numbers programmed by Leslie Silverman.\n"
-           "Ms Silverman has been working for the Longstreet Software Company for the last two years.\n");
-    double answer = compare();
-    // change cout to printf
-    std::cout << "Answer that was returned to driver " << answer << std::endl;
+    printf("[ Array!  hurray!  it's displayed ]\n");
     return 0;
 }
--- assignment-03/main.c
+++ assignment-03/main.c
@@ -2,13 +2,22 @@
 
 extern int sum();
 extern int input_array();
+extern int display_array();
 
 int main(int argc, char *argv[])
 {
-    printf("Welcome to Floating Points Numbers programmed by Leslie Silverman.\n"
-           "Ms Silverman has been working for the Longstreet Software Company for the last two years.\n");
+    printf("\n\n\nWelcome to Sum Array!\n"
+           "programmed by Chandra Lindy\n\n");
     int answer = sum();
-    int input_array();
-    printf("Answer that was return from sum(): %d", answer);
+    int result = input_array();
+    printf("Answer that was return from sum(): %d\n", answer);
+    printf("Return value from input_array(): %d\n", result);
+
+    printf("display_array works!!");
+    display_array();
+
+    printf("\n\nThank you for running this program!  Wish you the best for rest of the day...\n");
+    printf("main.c will return 0 to the operating system.  Buh bye!");
+
     return 0;
 }
